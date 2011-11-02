965d5e809c3083390176e57379761add33095165
Andrew Tomaka <atomaka@gmail.com>
2011-11-02 12:45:37 -0400

    My version: issue accessing img in the previewHtml.

--- js/upload.js
+++ js/upload.js
@@ -1,14 +1,38 @@
+var previewHtml = 
+'<div class="preview">' +
+	'<span class="imagePreview">' + 
+		'<img /><span class="imageSuccess"></span>' +
+	'</span>' +
+	'<div class="imageProgress"><div class="progressBar"></div></div>' +
+'</div>';
+
 $('#drop').filedrop({
 	url: 			'upload.php',
 	paramname: 		'file', 
 	maxfiles: 		4,
 	maxfilesize: 	1,
 
-	uploadFinished: function(i, file, response, time) {
-		
+	beforeEach: 		function(file) {
+		// alert('temp attempting upload')	;
 	},
-	beforeEach: 	function(file) {
-		alert('temp attempting upload')	;
+	uploadStarted: 		function(i, file, len) {
+		var reader = new FileReader();
+		
+		reader.onload = function(e) {
+			alert(e.target.result);
+			$('img',$(previewHtml)).attr('src', e.target.result);
+		}
+
+		reader.readAsDataURL(file);
+		$(previewHtml).appendTo($('#drop'));
+
+		$.data(file, $(previewHtml));l
+	},
+	progressUpdated: 	function(i, file, progress) {
+		$.data(file).find('.progressBar').width(progress);
+	}, 
+	uploadFinished: function(i, file, response, time) {
+		$.data(file).addClass('complete');
 	},
 
 	error: 			function(type, file) {
