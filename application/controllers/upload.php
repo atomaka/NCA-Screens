fc502c29681500f5d3cea0db2e8b6197ce879742
atomaka <atomaka@gmail.com>
2011-11-06 00:57:35 -0400

    Use CI built-in image manipulation library to create thumbnails.

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -10,10 +10,13 @@ class Upload extends CI_Controller {
 
 		$temp_name = md5(rand());
 
-		$config['file_name'] 		= $temp_name;
-		$config['max_size']			= 2048;
-		$config['upload_path']		= './uploads/';
-		$config['allowed_types']	= 'gif|jpg|jpeg|png|bmp';
+		$config = array(
+			'file_name'					=> $temp_name,
+			'max_size'					=> 2048,
+			'upload_path'				=> './uploads/',
+			'allowed_types'				=> 'gif|jpg|jpeg|png|bmp'
+		);
+
 		$this->load->library('upload', $config);
 
 		if (!$this->upload->do_upload('image')) {
@@ -23,8 +26,24 @@ class Upload extends CI_Controller {
 
 			$this->load->model('fileupload');
 			$file_name = $this->fileupload->add_upload($upload['file_ext'], $upload['client_name']);
-
 			rename($upload['full_path'], $upload['file_path'] . $file_name . $upload['file_ext']);
+
+			$config = array(
+				'image_library'			=> 'gd2',
+				'source_image'			=> $upload['file_path'] . $file_name . $upload['file_ext'],
+				'create_thumb'			=> true,
+				'maintain_ratio'		=> true,
+				'width'					=> 175,
+				'height'				=> 50,
+				'new_image'				=> './thumbs/' . $file_name . $upload['file_ext'],
+				'thumb_marker'			=> '',
+			);
+
+			$this->load->library('image_lib',$config);
+			
+			if($this->image_lib->resize() != true) echo 'dead to me.';
+
+			echo $this->image_lib->display_errors();
 
 			$message = array('type'=>'success','status'=>'Uploaded successfully', 'file'=>base_url('/view/specific/' . $file_name));
 		}
