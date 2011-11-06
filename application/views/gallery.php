58761af95f1b3fe398fd9d4988aadf8cb1292d78
atomaka <atomaka@gmail.com>
2011-11-06 02:40:37 -0500

    Minor style changes to gallery.

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -1,4 +1,6 @@
+		<div id="message" class="pagination">
 <?php echo $pagination; ?>
+		</div>
 
 		<div id="imageContainer" style="width: 960px;margin: auto;">
 <?php foreach($uploads as $upload) { ?>
@@ -6,4 +8,9 @@
 				<img src="<?php echo base_url('/thumbs/' . $upload->id . $upload->extension); ?>" />
 			</div>
 <?php } ?>
+		</div>
+		<div style="clear:both;"></div>
+
+		<div id="message" class="pagination">
+<?php echo $pagination; ?>
 		</div>
