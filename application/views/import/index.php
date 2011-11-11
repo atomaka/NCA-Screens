3ecfd60dfd189f292c32a0b8f8d61574874c2163
atomaka <atomaka@gmail.com>
2011-11-11 00:42:24 -0500

    Don't use framework for writing the throwaway import script.

--- application/views/import/index.php
+++
@@ -1,8 +0,0 @@
-	<div style="margin:auto; top: 150px;" class="ui-dialog ui-widget ui-widget-content ui-corner-all ui-draggable ui-resizable">
-	   <div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix">
-	      <span id="ui-dialog-title-dialog" class="ui-dialog-title">Confirmation</span>
-	   </div>
-	   <div style="width: auto;" class="ui-dialog-content ui-widget-content" id="dialog">
-	      <p>Are you sure you are ready to import?  <a href="<?php echo base_url('/import/process'); ?>" style="text-decoration: underline">Yes</a>!</p>
-	   </div>
-	</div>
