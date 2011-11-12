64066b7ea92b18d5091bf63288865e5c982e4919
atomaka <atomaka@gmail.com>
2011-11-11 20:39:17 -0500

    Deal with the first 926 thumbnails always being jpegs on the production site.

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -3,9 +3,13 @@
 		</div>
 
 		<div id="imageContainer" style="width: 960px;margin: auto;">
-<?php foreach($uploads as $upload) { ?>
+<?php 
+foreach($uploads as $upload) { 
+	$extension = (preg_match('/guild/',$_SERVER['HTTP_HOST']) != 0 && $upload->id <= 927)
+		? '.jpg' : $upload->extension;
+?>
 			<div id="galleryContainer">
-				<a href="<?php echo base_url($upload->id); ?>"><img src="<?php echo base_url('/thumbs/' . $upload->id . $upload->extension); ?>" /></a>
+				<a href="<?php echo base_url($upload->id); ?>"><img src="<?php echo base_url('/thumbs/' . $upload->id . $extension); ?>" /></a>
 			</div>
 <?php } ?>
 		</div>
